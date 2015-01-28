1b1d697639cc904122ec15fb89d1c23fd134579a
Rick Windham <rick.windham@gmail.com>
2015-01-28 14:19:38 -0500

    added Podfile workspace and DataSource

---
+++ EtsyLister/EtsyDataSource.h
@@ -0,0 +1,13 @@
+//
+//  EtsyDataSource.h
+//  EtsyLister
+//
+//  Created by Rick Windham on 1/28/15.
+//  Copyright (c) 2015 Rick Windham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EtsyDataSource : NSObject
+
+@end
---
+++ EtsyLister/EtsyDataSource.m
@@ -0,0 +1,13 @@
+//
+//  EtsyDataSource.m
+//  EtsyLister
+//
+//  Created by Rick Windham on 1/28/15.
+//  Copyright (c) 2015 Rick Windham. All rights reserved.
+//
+
+#import "EtsyDataSource.h"
+
+@implementation EtsyDataSource
+
+@end
